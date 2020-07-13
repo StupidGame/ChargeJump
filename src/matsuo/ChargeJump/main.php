fce5b4cc2e51d929bd9c3c72998431177f267130
matsuo877 <matsuo877@gmail.com>
2020-07-13 19:55:39 +0900

    first commit

---
+++ src/matsuo/ChargeJump/main.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace matsuo\ChargeJump;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerToggleSneakEvent;
+use pocketmine\event\player\PlayerJumpEvent;
+
+class main extends PluginBase implements  Listener{
+
+}
